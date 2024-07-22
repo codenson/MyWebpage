b6275d92b2afcb6a041d18898d0d7d38c13e111f
m g <Marouane Guerouji>
2024-07-22 04:52:38 -0400

    Small update.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 async function getWeather() {
   try {
     const response = await fetch(
-      "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+      "https://api.open-meteo.com/v1/forecast?latitude=38.8951&longitude=77.0364&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+      //"https://api.open-meteo.com/v1/forecast?latitude=38.9072&longitude=77.0369&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+      // "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
     );
 
     const weather = await response.json();
@@ -12,7 +14,7 @@ async function getWeather() {
     console.log("Current Temperature:", currentTemperature + "°C");
 
     document.querySelector(".p").innerHTML =
-      "Temperature in Berlin: " + currentTemperature + "°C";
+      "Berlin: " + currentTemperature + "°C";
 
     var time = weather.current.time;
     document.querySelector(".time").textContent = "As of " + time;
@@ -26,3 +28,31 @@ async function getWeather() {
 }
 
 getWeather();
+
+async function fetchBitcoinLatestPrice() {
+  const headers = { 'X-CoinAPI-Key': '61F81EC0-A2B1-40C4-8559-A2D1AD0D0A34' };
+  const URL = "https://rest.coinapi.io/v1/exchangerate/btc/usd";
+
+  try {
+    const response = await fetch(URL, { headers: headers });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    var price = Math.round(data.rate);
+
+    document.querySelector(".btc").textContent = "BTC: $" + price;
+
+  } catch (error) {
+    console.error("Error fetching Bitcoin price:", error);
+  }
+}
+
+fetchBitcoinLatestPrice();
+
+
+
+
+
+
